a5a3b8dd5c7c650ab6f539b51d41f9e7cd303a70
Paul Downey <psd@whatfettle.com>
2022-09-14 17:27:47 +0100

    Better range checking

--- bin/find-gaps.py
+++ bin/find-gaps.py
@@ -20,19 +20,26 @@ for path in glob.glob("content/dataset/*.md"):
 
 
 # initial range ..
-ranges = [[Decimal(1), Decimal(2199999)]]
+ranges = [
+    [Decimal(1), Decimal(999)],  # early assignments
+]
 
 for dataset, row in datasets.items():
-    ranges.append([Decimal(row.get("entity-minimum", "") or 0), Decimal(row.get("entity-maximum", "") or 0)])
+    ranges.append(
+        [
+            Decimal(row.get("entity-minimum", "") or 0),
+            Decimal(row.get("entity-maximum", "") or 0),
+        ]
+    )
 
 ranges = sorted(ranges)
-lowest = ranges[0][0]-1
-highest = ranges[-1][1]+1
+lowest = ranges[0][0] - 1
+highest = ranges[-1][1] + 1
 flat = chain((lowest,), chain.from_iterable(ranges), (highest,))
-gaps = [[x+1, y-1] for x, y in zip(flat, flat) if x+1 < y]
+gaps = [[x + 1, y - 1] for x, y in zip(flat, flat) if x + 1 < y]
 
 for gap in gaps:
     if gap[1] - gap[0] > 0:
-        print(gap[0], gap[1], gap[1]-gap[0])
+        print(gap[0], gap[1], gap[1] - gap[0])
 
 print(highest)
