6c71e46ebdd4eb6e9bebc28c4d32129b6db0c58b
Adam Shimali <adam.shimali@gmail.com>
2024-02-20 10:27:57 +0000

    Keep dataset-field-version.csv in it's own directory. Copy to specification directory, append and copy back to data directory.
    This is to maintain an append only history of dataset field versions.

--- bin/dataset-field.py
+++ bin/dataset-field.py
@@ -3,7 +3,7 @@
 import sys
 import csv
 import frontmatter
-import requests
+import shutil
 
 # create dataset-field.csv
 fieldnames = ["dataset", "field", "field-dataset", "guidance", "hint"]
@@ -34,21 +34,18 @@ for row in csv.DictReader(open("specification/dataset.csv", newline="")):
 # amend dataset-field-version.csv
 
 new_rows = []
-fieldnames_with_version = ["dataset", "field", "field-dataset", "guidance", "hint", "version"]
-
-def fetch_current_pubished_csv():
-    #TODO remove this once version number is moved to dataset-field.csv
-    github_url = 'https://raw.githubusercontent.com/digital-land/specification/main'
-    version_file_url = f'{github_url}/specification/dataset-field-version.csv'
-
-    # TBD: replace this, it doesn't work offline
-    resp = requests.get(version_file_url)
-    with open('specification/dataset-field-version.csv', 'w', newline='') as csvfile:
-        csvfile.write(resp.text)
+fieldnames_with_version = [
+    "dataset",
+    "field",
+    "field-dataset",
+    "guidance",
+    "hint",
+    "version",
+]
 
 
 def row_exists(csv_file, subset_values):
-    with open(csv_file, 'r', newline='') as csvfile:
+    with open(csv_file, "r", newline="") as csvfile:
         csv_reader = csv.DictReader(csvfile)
         for r in csv_reader:
             # Check if the subset_values are present in the row
@@ -56,20 +53,26 @@ def row_exists(csv_file, subset_values):
                 return True
         return False
 
+
 def version_major_number(version):
     return str(int(version))
 
+
 def append_rows(csv_file, fieldnames, rows):
-    with open(csv_file, 'a', newline='') as csvfile:
+    with open(csv_file, "a", newline="") as csvfile:
         csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         if csvfile.tell() == 0:
             csv_writer.writeheader()
 
         csv_writer.writerows(rows)
 
-# set dataset-field-version.csv to latest version
-fetch_current_pubished_csv()
-csv_file = 'specification/dataset-field-version.csv'
+
+# get current dataset-field-version.csv - otherwise build process deletes field version history
+shutil.copyfile(
+    "data/dataset-field-version.csv", "specification/dataset-field-version.csv"
+)
+
+dataset_field_version_file = "specification/dataset-field-version.csv"
 
 # determine if new dataset-field-version rows need to be added
 for row in csv.DictReader(open("specification/dataset.csv", newline="")):
@@ -78,15 +81,15 @@ for row in csv.DictReader(open("specification/dataset.csv", newline="")):
 
     post = frontmatter.load(path)
     # check schema has version number
-    if 'version' in post.metadata:
+    if "version" in post.metadata:
         version = version_major_number(post.metadata["version"])
         for field in post.metadata["fields"]:
             dataset_field_version_row = {
-                'dataset': dataset,
-                'field': field['field'],
-                'version': version
+                "dataset": dataset,
+                "field": field["field"],
+                "version": version,
             }
-            if not row_exists(csv_file, dataset_field_version_row):
+            if not row_exists(dataset_field_version_file, dataset_field_version_row):
                 new_rows.append(
                     {
                         "dataset": dataset,
@@ -94,9 +97,13 @@ for row in csv.DictReader(open("specification/dataset.csv", newline="")):
                         "guidance": field.get("guidance", ""),
                         "field-dataset": field.get("dataset", ""),
                         "hint": field.get("hint", ""),
-                        "version": version
+                        "version": version,
                     }
                 )
 
 if len(new_rows) > 0:
-    append_rows(csv_file, fieldnames_with_version, new_rows)
+    append_rows(dataset_field_version_file, fieldnames_with_version, new_rows)
+    # store copy of dataset-field-version.csv in data directory to maintain history
+    shutil.copyfile(
+        "specification/dataset-field-version.csv", "data/dataset-field-version.csv"
+    )
